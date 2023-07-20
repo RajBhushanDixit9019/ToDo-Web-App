be8b40c20473da000878c30441cb4acf6d4f2f43
Raj Bhushan Dixit <rajbhushandixit99@gmail.com>
2023-07-20 07:43:12 +0530

    Main app front-end interface is created, Basic functionality like adding list, adding tasks to the list is provided.

---
+++ Front-End/MainApp.js
@@ -0,0 +1,135 @@
+// Retrieve elements from the DOM
+const listContainer = document.getElementById('list-container');
+const newListInput = document.getElementById('new-list-input');
+const addListBtn = document.getElementById('add-list-btn');
+const listName = document.getElementById('list-name');
+const taskList = document.getElementById('task-list');
+const newTaskInput = document.getElementById('new-task-input');
+const addTaskBtn = document.getElementById('add-task-btn');
+const errorMessage = document.getElementById('error-message');
+
+// Data structure to store lists and tasks
+let listsData = [];
+
+// Function to initialize the app with a default list "My Day"
+function initializeApp() {
+    const defaultList = {
+        name: "My Day",
+        tasks: []
+    };
+    listsData.push(defaultList);
+    renderLists();
+    setSelectedListIndex(0);
+}
+
+// Event listeners for adding lists and tasks
+addListBtn.addEventListener('click', addList);
+addTaskBtn.addEventListener('click', addTask);
+newListInput.addEventListener('keyup', function(event) {
+    if (event.key === "Enter") {
+        addList();
+    }
+});
+newTaskInput.addEventListener('keyup', function(event) {
+    if (event.key === "Enter") {
+        addTask();
+    }
+});
+listContainer.addEventListener('click', selectList);
+
+// Function to add a new list
+function addList() {
+    const listNameValue = newListInput.value.trim();
+
+    if (listNameValue) {
+        const newList = {
+            name: listNameValue,
+            tasks: []
+        };
+        listsData.push(newList);
+        renderLists();
+        newListInput.value = '';
+    }
+}
+
+// Function to render the list items in the sidebar
+function renderLists() {
+    listContainer.innerHTML = '';
+
+    listsData.forEach((list, index) => {
+        const newList = document.createElement('li');
+        newList.textContent = list.name;
+        newList.dataset.index = index;
+        newList.classList.toggle('active', index === selectedListIndex());
+        listContainer.appendChild(newList);
+    });
+
+    renderTasks();
+}
+
+// Function to handle list selection
+function selectList(event) {
+    if (event.target.tagName === 'LI') {
+        const listIndex = parseInt(event.target.dataset.index);
+        setSelectedListIndex(listIndex);
+        renderLists();
+    }
+}
+
+// Function to get the index of the selected list
+function selectedListIndex() {
+    const index = parseInt(localStorage.getItem('selectedListIndex'));
+    if (isNaN(index) || index >= listsData.length) {
+        setSelectedListIndex(0);
+        return 0;
+    }
+    return index;
+}
+
+// Function to set the index of the selected list
+function setSelectedListIndex(index) {
+    localStorage.setItem('selectedListIndex', index);
+}
+
+// Function to add a new task
+function addTask() {
+    const taskName = newTaskInput.value.trim();
+
+    if (taskName) {
+        const index = selectedListIndex();
+        if (index >= 0) {
+            const selectedList = listsData[index];
+            selectedList.tasks.push({ name: taskName });
+            renderTasks();
+            newTaskInput.value = '';
+            errorMessage.textContent = '';
+        } else {
+            errorMessage.textContent = 'Please select a list first.';
+        }
+    }
+}
+
+// Function to render the tasks in the main area
+function renderTasks() {
+    const index = selectedListIndex();
+    const selectedList = listsData[index];
+    listName.textContent = selectedList ? selectedList.name : '';
+    taskList.innerHTML = '';
+
+    if (selectedList) {
+        selectedList.tasks.forEach((task, index) => {
+            const newTask = document.createElement('li');
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.checked = false;
+            const label = document.createElement('label');
+            label.appendChild(checkbox);
+            label.appendChild(document.createTextNode(task.name));
+            newTask.appendChild(label);
+            taskList.appendChild(newTask);
+        });
+    }
+}
+
+// Initial rendering of lists and tasks
+initializeApp();
